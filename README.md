d93b59adc139b7a460e36e7aad474bd9d18d296a
ThalikStormrage <cwraiz@icloud.com>
2023-03-20 13:36:38 -0500

    Created Rock Paper Scissors Game, Added button to play

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# rock-paper-scissors
+# rock-paper-scissors//const choices = ['rock', 'paper', 'scissors'];
+//const computerSelection = getComputerChoice();
+
+//function getComputerChoice(){
+    //let randomComputerChoice = choices[Math.floor(Math.random() * choices.length)];
+    //return randomComputerChoice;
+//}
+//function playRound(playerSelection, computerSelection){
+    //if (playerSelection !== 'rock' || 'paper' || 'scissors'){
+    //    return 'Please choose rock paper or scissors'
+    //}
+    //else if (playerSelection =='rock' && computerSelection == 'paper'
+    //|| playerSelection == 'paper' && computerSelection == 'scissors'
+    //|| playerSelection == 'scissors' && computerSelection == 'rock') {
+    //    return `You Lose! ${playerSelection} beats ${computerSelection}`
+    //}
+    //else {
+    //    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    //}  
+//}
+
+//function game(games){
+    //for (let i = 0; i < games; i++) {
+            //let playerSelection = prompt('rock paper scissors');
+            //playerSelection.toLowerCase();
+            //result = playRound(playerSelection, computerSelection);
+            //console.log(result);
+        //}
+     //}
--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,13 +1,61 @@
-const game = ['rock', 'paper', 'scissors'];
+//global variables
+let userPrompt;
+let user = 0;
+let computer = 0;
 
-function getComputerChoice(){
-    let randomComputerChoice = game[Math.floor(Math.random() * game.length)];
+//Computer Randomized Selection Function
+function computerSelection(){
+    const choices = ['rock', 'paper', 'scissors'];
+    let randomComputerChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomComputerChoice;
 }
 
-let computerChoice = getComputerChoice();
+//Deciding a winner/loser Function
+function playRound(){
+    let userPrompt = prompt('Rock, Paper, or Scissors');
+    if (userPrompt === ""){
+        console.log('Enter either Rock Paper or Scissors');
+        playRound();
+    }
+    let userChoice = userPrompt.toLowerCase();
+    // Makes sure either rock, paper, or scissors was entered
+    if (userChoice != 'rock' && userChoice != 'paper' && userChoice != 'scissors'){
+        console.log('Enter either Rock Paper or Scissors');
+        playRound();
+    }
+    let computerChoice = computerSelection();
+    if (userChoice == 'rock' && computerChoice == 'scissors'
+     || userChoice == 'paper' && computerChoice == 'rock'
+     || userChoice == 'scissors' && computerChoice == 'paper'){
+        console.log(`You win! ${userChoice} beats ${computerChoice}`);
+        console.log(`User is at ${user += 1} and Computer is at ${computer}` );
+     }
+    else if (userChoice == 'rock' && computerChoice == 'paper'
+    || userChoice == 'paper' && computerChoice == 'scissors'
+    || userChoice == 'scissors' && computerChoice == 'rock'){
+        console.log(`You lost! ${computerChoice} beats ${userChoice}`);
+        console.log(`User is at ${user} and Computer is at ${computer += 1}` );
+    }
+    else if (userChoice == computerChoice){
+        console.log(`You both chose ${userChoice}, it's a draw`);
+        console.log(`User is at ${user} and Computer is at ${computer}` );
+    }
+    return user, computer;
+}
 
-console.log(computerChoice);
-
-
-
+function game(){
+    for (let i=0; i < 5; i++){
+        playRound();
+        if (i==4){
+            if (user>computer){
+                console.log('Game Over, The User wins!');
+            }
+            else if (user<computer){
+                console.log('Game Over, The Computer wins!');
+            }
+            else {
+                console.log('Game Over, It\'s a draw!');
+            }
+        }
+    }
+}
